000d815bc5a0b5a1f226b8fdb43148cca39e28b3
behavian <chaoshaowei@gmail.com>
2021-05-14 00:57:33 +0800

    parse html in progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from html.parser import HTMLParser
 
 #-----------------------------
 # directories
@@ -12,9 +13,11 @@ OUTPUT_DIR = os.path.join(WORKING_DIR, 'response.html')
 #-----------------------------
 # target url
 #-----------------------------
-url = 'https://cool.ntu.edu.tw/courses/4641'
+HOST = 'https://cool.ntu.edu.tw'
+PATH = '/courses/4641'
+URL = HOST+PATH
 
-
+'''
 #-----------------------------
 # directories
 #-----------------------------
@@ -35,8 +38,59 @@ with open(HEADER_DIR, 'r') as f:
 # get course homepage response
 #-----------------------------
 s = requests.Session() 
-r = s.get(url, headers=headers)
+r = s.get(URL, headers=headers)
 
 with open(OUTPUT_DIR, 'w', encoding='utf-8') as f:
     f.write(r.text)
 
+'''
+
+with open(OUTPUT_DIR, 'r', encoding='utf-8') as f:
+    text = '\n'.join(f.readlines())
+
+#-----------------------------
+# parse homepage
+#-----------------------------
+class HomepageParser(HTMLParser):
+    depth = 0
+    in_module_body = False
+    module_body_depth = 0
+
+    item_next_type = 'NAN'
+
+    def handle_starttag(self, tag, attrs):
+        attrs_dict = dict(attrs)
+        if (tag == 'div'):
+            self.depth += 1
+            # Find module
+            if 'id' in attrs_dict:
+                if (attrs_dict['id'] == 'context_modules'):
+                    self.in_module_body = True
+                    self.module_body_depth = self.depth
+                    print(f'found module at div depth ({self.depth})')
+            if 'class' in attrs_dict:
+                # Find topics
+                if (attrs_dict['class'].startswith('item-group-condensed')):
+                    print(f"\tTopic: {attrs_dict['aria-label']}")
+                # Find individual items
+                elif (attrs_dict['class'] == 'module-item-title'):
+                    print(f"\t\tfound {self.item_next_type}")
+                    self.item_next_type = 'NAN'
+        if (tag == 'span'):
+            if 'class' in attrs_dict:
+                if attrs_dict['class'] == 'type_icon':
+                    self.item_next_type = attrs_dict['title']
+
+    def handle_endtag(self, tag):
+        if (self.in_module_body):
+            if (self.depth == self.module_body_depth):
+                self.in_module_body = False
+        if (tag == 'div'):
+            self.depth -= 1
+
+    def handle_data(self, data):
+        return
+
+parser = HomepageParser()
+parser.feed(text)
+#parser.feed(r.text)
